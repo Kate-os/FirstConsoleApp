8336579e1e0d7a7cda953b9c3039fa77ca68349b
c.ososova@gmail.com <c.ososova@gmail.com>
2023-05-13 23:34:56 +0500

    Code edited

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,3 +1,9 @@
-﻿const string MyName = "Jane";
-Console.WriteLine(MyName);
-Console.ReadKey();
+﻿string MyName = "Jane";
+byte MyAge = 27;
+bool HaveIApet = true;
+double MyShoeSize = 37.5;
+
+Console.WriteLine("My name is " + MyName);
+Console.WriteLine("MyAge " + MyAge);
+Console.WriteLine("Do I have a pet? " + HaveIApet);
+Console.WriteLine("My shoe size is " + MyShoeSize);
